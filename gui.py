36ea657c47b7dc323839c50c5b013463ee18b4cf
Gules George Paul <paul.gules@gmail.com>
2018-05-18 17:58:53 +0200

    Add some functionalities when opening an g-code file, now it can print to stdout the x and y coordinates

--- gui.py
+++ gui.py
@@ -5,24 +5,23 @@ import design  # This file holds our MainWindow and all design related things
 
 # it also keeps events etc that we defined in Qt Designer
 import os  # For listing directory methods
-
+import graphicsview
+import re
 
 class ExampleApp(QtGui.QMainWindow, design.Ui_MainWindow):
     def __init__(self):
-        # Explaining super is out of the scope of this article
-        # So please google it if you're not familar with it
-        # Simple reason why we use it here is that it allows us to
-        # access variables, methods etc in the design.py file
         super(self.__class__, self).__init__()
         self.setupUi(self)  # This is defined in design.py file automatically
-        # It sets up layout and widgets that are defined
-        #self.btnBrowse.clicked.connect(self.browse_folder)  # When the button is pressed
-                                                            # Execute browse_folder function
+
+
         ##### Action Open
         openFile = QtGui.QAction(None)
         self.actionOpen.setShortcut('Ctrl+O')
         self.actionOpen.setStatusTip('Open application')
         self.actionOpen.triggered.connect(self.file_open)
+
+        #Button Circle Draw
+        self.Circle.clicked.connect(self.circleDraw)
 
         #action About
         self.actionAbout_GUI.triggered.connect(self.about)
@@ -39,8 +38,37 @@ class ExampleApp(QtGui.QMainWindow, design.Ui_MainWindow):
         self.actionSave.setStatusTip("Save Application")
         self.actionSave.triggered.connect(self.file_save)
 
+    def circleDraw(self):
+        print("Circle drawed!")
+        gcode = [
+            ';TYPE:SKIN',
+            'G1 F1200 X-9.914 Y-9.843 E3.36222',
+            'G0 F9000 X-9.843 Y-9.914',
+            'G1 F1200 X9.914 Y9.843 E3.65844',
+            'G0 F9000 X9.914 Y9.702',
+            'G1 F1200 X-9.702 Y-9.914 E3.95254',
+            'G0 F9000 X-9.560 Y-9.914',
+            'G1 F1200 X9.914 Y9.560 E4.24451',
+            'G0 F9000 X9.914 Y9.419',
+            'G1 F1200 X-9.419 Y-9.914 E4.53437',
+            'G0 F9000 X-9.277 Y-9.914',
+            'G1 F1200 X9.914 Y9.277 E4.82211',
+            'G0 F9000 X9.914 Y9.136',
+            'G1 F1200 X-9.136 Y-9.914 E5.10772',
+            'G0 F9000 X-8.995 Y-9.914',
+            'G1 F1200 X9.914 Y8.995 E5.39123',
+            'G0 F9000 X9.914 Y8.853',
+            'G1 F1200 X-8.853 Y-9.914 E5.67260'
+            ]
+
+        for line in gcode:
+            coord = re.findall(r'[XY]-\d.\d\d\d', line)
+            if coord:
+                print("{} - {}".format(coord[0], coord[1]))
+
+
     def file_save(self):
-        name = QtGui.QFileDialog.getSaveFileName(self, 'Save File')
+        name = QtGui.QFileDialog.getSaveFileName(self, 'Save File','/home/paul/EZB/Project/GUI')
         file = open(name, 'w')
         text = self.textEdit.toPlainText()
         file.write(text)
@@ -48,16 +76,17 @@ class ExampleApp(QtGui.QMainWindow, design.Ui_MainWindow):
 
     def file_open(self):
         fname = QtGui.QFileDialog.getOpenFileName(self, 'Open file',
-                '/home')
+                '/home/paul/EZB/Project/GUI')
 
-        f = open(fname, 'r')
-
-        with f:
-            data = f.read()
-            self.textEdit.setText(data)
+        f = open('circle.gcode', 'r')
+        for line in f:
+            coord = re.findall(r'[XY]-\d.\d\d\d', line)
+            if coord:
+                print("{} - {}".format(coord[0], coord[1]))
 
     def about(self):
         QtGui.QMessageBox.about(None,"About","George-Paul GUI-Interface 2018")
+
 
 
 def main():
