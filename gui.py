4c7deed28f03e11e7651c06776ab5501f452af78
Gules George Paul <paul.gules@gmail.com>
2018-05-22 14:12:03 +0200

    Added some functionalities to open file

--- gui.py
+++ gui.py
@@ -1,18 +1,27 @@
-from PyQt4 import QtGui  # Import the PyQt4 module we'll need
+from PyQt4 import QtGui, QtCore  # Import the PyQt4 module we'll need
 import sys  # We need sys so that we can pass argv to QApplication
-
 import design  # This file holds our MainWindow and all design related things
 
 # it also keeps events etc that we defined in Qt Designer
-import os  # For listing directory methods
+import os,re  # For listing directory methods
 import graphicsview
-import re
 
 class ExampleApp(QtGui.QMainWindow, design.Ui_MainWindow):
     def __init__(self):
         super(self.__class__, self).__init__()
         self.setupUi(self)  # This is defined in design.py file automatically
 
+        #QtGui.QGraphicsView.__init__(self)
+        self.scene = QtGui.QGraphicsScene(self)
+        self.item = QtGui.QGraphicsEllipseItem(-20, -10, 40, 20)
+        self.scene.addItem(self.item)
+        #self.setScene(self.scene)
+
+        self.tl = QtCore.QTimeLine(1000)
+        self.tl.setFrameRange(0, 100)
+        self.a = QtGui.QGraphicsItemAnimation()
+        self.a.setItem(self.item)
+        self.a.setTimeLine(self.tl)
 
         ##### Action Open
         openFile = QtGui.QAction(None)
@@ -20,6 +29,8 @@ class ExampleApp(QtGui.QMainWindow, design.Ui_MainWindow):
         self.actionOpen.setStatusTip('Open application')
         self.actionOpen.triggered.connect(self.file_open)
 
+        #Button Rectangle Draw
+        self.Rectangle.clicked.connect(self.rectangleDraw)
         #Button Circle Draw
         self.Circle.clicked.connect(self.circleDraw)
 
@@ -38,8 +49,27 @@ class ExampleApp(QtGui.QMainWindow, design.Ui_MainWindow):
         self.actionSave.setStatusTip("Save Application")
         self.actionSave.triggered.connect(self.file_save)
 
+
+
+    def rectangleDraw(self):
+        gcode = [
+            ';TYPE:SKIN',
+            'G00 X-0 Y-0 F70',
+            'G01 Z-1 F50',
+            'G01 X0 Y20 F50',
+            'G01 X25 Y20',
+            'G01 X25 Y0',
+            'G01 X0 Y0',
+            'G00 Z0 F70',
+            'M30'
+            ]
+
+        for line in gcode:
+            coord = re.findall(r'[XY]-\d.\d\d\d', line)
+            if coord:
+                print("{} - {}".format(coord[0], coord[1]))
+
     def circleDraw(self):
-        print("Circle drawed!")
         gcode = [
             ';TYPE:SKIN',
             'G1 F1200 X-9.914 Y-9.843 E3.36222',
@@ -78,6 +108,11 @@ class ExampleApp(QtGui.QMainWindow, design.Ui_MainWindow):
         fname = QtGui.QFileDialog.getOpenFileName(self, 'Open file',
                 '/home/paul/EZB/Project/GUI')
 
+        txt_files = [f for f in os.listdir('.') if f.endswith('.gcode')]
+        if len(txt_files) < 1:
+            raise ValueError('There is no file with gcode extensions!')
+
+            filename = txt_files[0]
         f = open('circle.gcode', 'r')
         for line in f:
             coord = re.findall(r'[XY]-\d.\d\d\d', line)
