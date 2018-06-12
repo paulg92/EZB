478ad4c2f05e0cc3ca13aba27d5452737296ebfb
Gules George Paul <paul.gules@gmail.com>
2018-06-12 19:42:10 +0200

    Now you can draw in the Graphiv design of the program with the mouse

--- gui.py
+++ gui.py
@@ -1,27 +1,147 @@
 from PyQt4 import QtGui, QtCore  # Import the PyQt4 module we'll need
-import sys  # We need sys so that we can pass argv to QApplication
+import sys, math # We need sys so that we can pass argv to QApplication
 import design  # This file holds our MainWindow and all design related things
-
+from PyQt4.QtGui import QPainter
 # it also keeps events etc that we defined in Qt Designer
 import os,re  # For listing directory methods
 import graphicsview
 
-class ExampleApp(QtGui.QMainWindow, design.Ui_MainWindow):
-    def __init__(self):
+
+class GraphicsView(QtGui.QWidget):
+    """
+      this scales the image but it's not good, too many refreshes really mess it up!!!
+    """
+    def __init__(self, parent=None):
+        super(GraphicsView, self).__init__(parent)
+
+        self.setAttribute(QtCore.Qt.WA_StaticContents)
+        self.modified = False
+        self.scribbling = False
+        self.myPenWidth = 1
+        self.myPenColor = QtCore.Qt.blue
+        imageSize = QtCore.QSize(500, 500)
+        self.image = QtGui.QImage(imageSize, QtGui.QImage.Format_RGB32)
+        self.lastPoint = QtCore.QPoint()
+
+    def openImage(self, fileName):
+        loadedImage = QtGui.QImage()
+        if not loadedImage.load(fileName):
+            return False
+
+        w = loadedImage.width()
+        h = loadedImage.height()
+        self.mainWindow.resize(w, h)
+
+#       newSize = loadedImage.size().expandedTo(self.size())
+#       self.resizeImage(loadedImage, newSize)
+        self.image = loadedImage
+        self.modified = False
+        self.update()
+        return True
+
+    def saveImage(self, fileName, fileFormat):
+        visibleImage = self.image
+        self.resizeImage(visibleImage, self.size())
+
+        if visibleImage.save(fileName, fileFormat):
+            self.modified = False
+            return True
+        else:
+            return False
+
+    def setPenColor(self, newColor):
+        self.myPenColor = newColor
+
+    def setPenWidth(self, newWidth):
+        self.myPenWidth = newWidth
+
+    def clearImage(self):
+        self.image.fill(QtGui.qRgb(255, 255, 255))
+        self.modified = True
+        self.update()
+
+    def mousePressEvent(self, event):
+        if event.button() == QtCore.Qt.LeftButton:
+            self.lastPoint = event.pos()
+            self.scribbling = True
+
+    def mouseMoveEvent(self, event):
+        if (event.buttons() & QtCore.Qt.LeftButton) and self.scribbling:
+            self.drawLineTo(event.pos())
+
+    def mouseReleaseEvent(self, event):
+        if event.button() == QtCore.Qt.LeftButton and self.scribbling:
+            self.drawLineTo(event.pos())
+            self.scribbling = False
+
+    def paintEvent(self, event):
+        painter = QtGui.QPainter(self)
+        painter.drawImage(event.rect(), self.image)
+
+    def resizeEvent(self, event):
+
+        self.resizeImage(self.image, event.size())
+
+        super(GraphicsView, self).resizeEvent(event)
+
+    def drawLineTo(self, endPoint):
+        painter = QtGui.QPainter(self.image)
+        painter.setPen(QtGui.QPen(self.myPenColor, self.myPenWidth,
+            QtCore.Qt.SolidLine, QtCore.Qt.RoundCap, QtCore.Qt.RoundJoin))
+        painter.drawLine(self.lastPoint, endPoint)
+        self.modified = True
+
+        self.update()
+        self.lastPoint = QtCore.QPoint(endPoint)
+
+    def resizeImage(self, image, newSize):
+        if image.size() == newSize:
+            return
+
+# this resizes the canvas without resampling the image
+        newImage = QtGui.QImage(newSize, QtGui.QImage.Format_RGB32)
+        newImage.fill(QtGui.qRgb(255, 255, 255))
+        painter = QtGui.QPainter(newImage)
+        painter.drawImage(QtCore.QPoint(0, 0), image)
+
+
+        self.image = newImage
+
+    def print_(self):
+        printer = QtGui.QPrinter(QtGui.QPrinter.HighResolution)
+
+        printDialog = QtGui.QPrintDialog(printer, self)
+        if printDialog.exec_() == QtGui.QDialog.Accepted:
+            painter = QtGui.QPainter(printer)
+            rect = painter.viewport()
+            size = self.image.size()
+            size.scale(rect.size(), QtCore.Qt.KeepAspectRatio)
+            painter.setViewport(rect.x(), rect.y(), size.width(), size.height())
+            painter.setWindow(self.image.rect())
+            painter.drawImage(0, 0, self.image)
+            painter.end()
+
+    def isModified(self):
+        return self.modified
+
+    def penColor(self):
+        return self.myPenColor
+
+    def penWidth(self):
+        return self.myPenWidth
+
+
+class ExampleApp(QtGui.QMainWindow, design.Ui_MainWindow, QtGui.QGraphicsView):
+    def __init__(self, parent = None):
         super(self.__class__, self).__init__()
+        QtGui.QMainWindow.__init__(self, parent = parent)
         self.setupUi(self)  # This is defined in design.py file automatically
 
-        #QtGui.QGraphicsView.__init__(self)
-        self.scene = QtGui.QGraphicsScene(self)
-        self.item = QtGui.QGraphicsEllipseItem(-20, -10, 40, 20)
-        self.scene.addItem(self.item)
-        #self.setScene(self.scene)
+        self.graphicsView = GraphicsView(self)
+        self.graphicsView.clearImage()
+        self.graphicsView.mainWindow = self  # maybe not using this?
+        self.setCentralWidget(self.graphicsView)
 
-        self.tl = QtCore.QTimeLine(1000)
-        self.tl.setFrameRange(0, 100)
-        self.a = QtGui.QGraphicsItemAnimation()
-        self.a.setItem(self.item)
-        self.a.setTimeLine(self.tl)
 
         ##### Action Open
         openFile = QtGui.QAction(None)
@@ -31,8 +151,8 @@ class ExampleApp(QtGui.QMainWindow, design.Ui_MainWindow):
 
         #Button Rectangle Draw
         self.Rectangle.clicked.connect(self.rectangleDraw)
-        #Button Circle Draw
-        self.Circle.clicked.connect(self.circleDraw)
+        #Button Triangle Draw
+        self.Triangle.clicked.connect(self.triangleDraw)
 
         #action About
         self.actionAbout_GUI.triggered.connect(self.about)
@@ -48,8 +168,6 @@ class ExampleApp(QtGui.QMainWindow, design.Ui_MainWindow):
         self.actionSave.setShortcut('Ctrl+S')
         self.actionSave.setStatusTip("Save Application")
         self.actionSave.triggered.connect(self.file_save)
-
-
 
     def rectangleDraw(self):
         gcode = [
@@ -69,7 +187,7 @@ class ExampleApp(QtGui.QMainWindow, design.Ui_MainWindow):
             if coord:
                 print("{} - {}".format(coord[0], coord[1]))
 
-    def circleDraw(self):
+    def triangleDraw(self):
         gcode = [
             ';TYPE:SKIN',
             'G1 F1200 X-9.914 Y-9.843 E3.36222',
@@ -96,7 +214,6 @@ class ExampleApp(QtGui.QMainWindow, design.Ui_MainWindow):
             if coord:
                 print("{} - {}".format(coord[0], coord[1]))
 
-
     def file_save(self):
         name = QtGui.QFileDialog.getSaveFileName(self, 'Save File','/home/paul/EZB/Project/GUI')
         file = open(name, 'w')
@@ -112,8 +229,8 @@ class ExampleApp(QtGui.QMainWindow, design.Ui_MainWindow):
         if len(txt_files) < 1:
             raise ValueError('There is no file with gcode extensions!')
 
-            filename = txt_files[0]
-        f = open('circle.gcode', 'r')
+        filename = txt_files[0]
+        f = open('eample.gcode', 'r')
         for line in f:
             coord = re.findall(r'[XY]-\d.\d\d\d', line)
             if coord:
