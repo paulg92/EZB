9ef420958038b09f64a1c94f51554dbb35c5f071
Gules George Paul <paul.gules@gmail.com>
2018-06-17 16:46:46 +0200

    splitted the python files for better readability

---
+++ buttons.py
@@ -0,0 +1,82 @@
+
+from PyQt4 import QtGui, QtCore
+
+class GraphicsView(QtGui.QWidget):
+    def __init__(self, parent=None):
+        super(GraphicsView, self).__init__(parent)
+
+        self.setAttribute(QtCore.Qt.WA_StaticContents)
+        self.modified = False
+        self.scribbling = False
+        self.myPenWidth = 1
+        self.myPenColor = QtCore.Qt.blue
+        imageSize = QtCore.QSize(500, 500)
+        self.image = QtGui.QImage(imageSize, QtGui.QImage.Format_RGB32)
+        self.lastPoint = QtCore.QPoint()
+
+    def setPenColor(self, newColor):
+        self.myPenColor = newColor
+
+    def setPenWidth(self, newWidth):
+        self.myPenWidth = newWidth
+
+    def clearImage(self):
+        self.image.fill(QtGui.qRgb(255, 255, 255))
+        self.modified = True
+        self.update()
+
+    def mousePressEvent(self, event):
+        if event.button() == QtCore.Qt.LeftButton:
+            self.lastPoint = event.pos()
+            self.scribbling = True
+
+    def mouseMoveEvent(self, event):
+        if (event.buttons() & QtCore.Qt.LeftButton) and self.scribbling:
+            self.drawLineTo(event.pos())
+
+    def mouseReleaseEvent(self, event):
+        if event.button() == QtCore.Qt.LeftButton and self.scribbling:
+            self.drawLineTo(event.pos())
+            self.scribbling = False
+
+    def paintEvent(self, event):
+        painter = QtGui.QPainter(self)
+        painter.drawImage(event.rect(), self.image)
+
+    def resizeEvent(self, event):
+
+        self.resizeImage(self.image, event.size())
+
+        super(GraphicsView, self).resizeEvent(event)
+
+    def drawLineTo(self, endPoint):
+        painter = QtGui.QPainter(self.image)
+        painter.setPen(QtGui.QPen(self.myPenColor, self.myPenWidth,
+            QtCore.Qt.SolidLine, QtCore.Qt.RoundCap, QtCore.Qt.RoundJoin))
+        painter.drawLine(self.lastPoint, endPoint)
+        self.modified = True
+
+        self.update()
+        self.lastPoint = QtCore.QPoint(endPoint)
+
+
+    def resizeImage(self, image, newSize):
+        if image.size() == newSize:
+            return
+
+# this resizes the canvas without resampling the image
+        newImage = QtGui.QImage(newSize, QtGui.QImage.Format_RGB32)
+        newImage.fill(QtGui.qRgb(255, 255, 255))
+        painter = QtGui.QPainter(newImage)
+        painter.drawImage(QtCore.QPoint(0, 0), image)
+
+        self.image = newImage
+
+    def isModified(self):
+        return self.modified
+
+    def penColor(self):
+        return self.myPenColor
+
+    def penWidth(self):
+        return self.myPenWidth
